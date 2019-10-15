e5ada296c9b14e0b5f70e0c225035925af7c794e
mingjian <mingjian_zhang@qq.com>
2019-10-15 22:31:54 +0800

    CodeDomProvider test

---
+++ src/DynamicProxies/ProxyGenerator.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+
+namespace Rapidity.Http.DynamicProxies
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ProxyGenerator
+    {
+        public static Type Generate(Type type)
+        {
+            //var options = new Dictionary<string, string> {
+            //    { "CompilerVersion", "v4.5" }
+            //};
+            //CSharpCodeProvider provider = new CSharpCodeProvider();
+            var provider = CodeDomProvider.CreateProvider("CSharp");
+            var referencedDll = new string[]
+            {
+                "mscorlib.dll",
+                "System.dll",
+                "System.Core.dll",
+            };
+            var parameters = new CompilerParameters(referencedDll)
+            {
+                IncludeDebugInformation = true,
+                GenerateInMemory = true,
+                TreatWarningsAsErrors = true
+            };
+            var source = new[] { "class Test { static void Foo() {}}" };
+            var result = provider.CompileAssemblyFromSource(parameters, source);
+            if (result.Errors.HasErrors)
+            {
+                var error = result.Errors[0];
+                throw new Exception(error.ToString());
+            }
+            
+            var typerest = result.CompiledAssembly.GetType("Test");
+            return typerest;
+        }
+    }
+}
---
+++ tests/Rapidity.Http.Tests/ProxyGeneratorTest.cs
@@ -0,0 +1,17 @@
+﻿using Rapidity.Http.DynamicProxies;
+using Xunit;
+
+namespace Rapidity.Http.Tests
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ProxyGeneratorTest
+    {
+        [Fact]
+        public void GeneratorTest()
+        {
+            var type = ProxyGenerator.Generate(typeof(string));
+        }
+    }
+}
